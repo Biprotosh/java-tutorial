3c3a93d56d7bdef444fc416d1f9d1a33e86d97a9
Biprotosh <biprotosh123@gmail.com>
2026-05-19 12:54:18 +0530

    Trying push command in intellijIdea

--- src/ObjectClass/ObjectClass.java
+++ src/ObjectClass/ObjectClass.java
@@ -4,7 +4,7 @@ public class ObjectClass {
     public static void main(String[] args) {
         ExampleStudent s1 = new ExampleStudent();
 
-//        System.out.println(s1.toString());
+        System.out.println(s1.toString());
         System.out.println(s1); // println by default prints toString() method
     }
 }
