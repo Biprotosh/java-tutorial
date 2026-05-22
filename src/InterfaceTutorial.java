5c3aae574b3fbd68aee96441e07a863bdbb44213
Biprotosh <biprotosh123@gmail.com>
2026-05-22 21:14:46 +0530

    deep dive in interfaces part 2. Learnt how to solve diamond problem using interface, resolution priority rule and special type of interfaces

--- src/InterfaceTutorial.java
+++ src/InterfaceTutorial.java
@@ -5,6 +5,14 @@ public class InterfaceTutorial {
         person.walk();
         person.seat();
         person.eat();
+
+        // Solving diamond problem using interface
+        X xyz = new XYZ();
+        xyz.fun();
+
+        // Resolution priority
+        Hi h = new Hi();
+        h.fun();
     }
 }
 
@@ -31,4 +39,49 @@ class Human implements Seatable, MathConstants{
     public void walk(){
         System.out.println("Walking");
     }
+}
+
+
+// Solving diamond problem using interface
+interface X {
+    void fun();
+}
+
+interface Y extends X{
+//    void fun();
+
+    @Override
+    default void fun(){
+        System.out.println("Interface Y");
+    }
+}
+
+interface Z extends X{
+//    void fun();
+
+    @Override
+    default void fun(){
+        System.out.println("Interface Z");
+    }
+}
+
+class XYZ implements Y,Z{
+    @Override
+    public void fun(){
+//        System.out.println("Default Implementation");
+
+        // want to use B/C implementation
+        Y.super.fun();
+        Z.super.fun();
+    }
+}
+
+class Hello {
+    public void fun(){
+        System.out.println("Inside Hello class");
+    }
+}
+
+class Hi extends Hello implements Z{
+    // if we call fun() and both the interface and class has the duplicate method then java gives the priority to extended class's implementation
 }
